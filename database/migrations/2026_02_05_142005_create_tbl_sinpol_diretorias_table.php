a36925a4f354d611a1c7c2e174aff4d30659f838
Dartanhan <dartanhan.lima@gmail.com>
2026-03-09 16:25:17 -0300

    feat: create `tbl_sinpol_diretorias` and `tbl_sinpol_paginas` tables

--- database/migrations/2026_02_05_142005_create_tbl_sinpol_diretorias_table.php
+++ database/migrations/2026_02_05_142005_create_tbl_sinpol_diretorias_table.php
@@ -13,14 +13,16 @@ class CreateTblSinpolDiretoriasTable extends Migration
      */
     public function up()
     {
-        Schema::create('tbl_sinpol_diretorias', function (Blueprint $table) {
-            $table->id()->autoIncrement()->comment("identificação da diretoria");
-            $table->longText('conteudo')->nullable(false)->comment("conteudo do tiny mce");
-            $table->string('titulo', 150)->nullable(false)->comment('titulo da diretoria');
-            $table->string('slug', 155)->nullable(false)->comment("slug da diretoria");
-            $table->boolean('status')->default(false)->comment("ativa ou desativa a diretoria para ser exibida no site");
-            $table->timestamps();
-        });
+        if (!Schema::hasTable('tbl_sinpol_diretorias')) {
+            Schema::create('tbl_sinpol_diretorias', function (Blueprint $table) {
+                $table->id()->autoIncrement()->comment("identificação da diretoria");
+                $table->longText('conteudo')->nullable(false)->comment("conteudo do tiny mce");
+                $table->string('titulo', 150)->nullable(false)->comment('titulo da diretoria');
+                $table->string('slug', 155)->nullable(false)->comment("slug da diretoria");
+                $table->boolean('status')->default(false)->comment("ativa ou desativa a diretoria para ser exibida no site");
+                $table->timestamps();
+            });
+        }
     }
 
     /**
